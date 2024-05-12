d7272d677934e8d17d1ad73339a60bcd9a92e851
Yaw Owusu Kessie <122237149+kessiey@users.noreply.github.com>
2024-05-12 17:27:55 +0000

    first commit

---
+++ script.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e  # Exit script immediately if a command fails
+set -o pipefail  # Exit status of last command that threw a non-zero exit code
+
+# Install Terraform
+sudo apt update
+sudo apt install -y wget gnupg
+wget -qO- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list > /dev/null
+sudo apt update && sudo apt install terraform -y
+
+# Install kubectl
+sudo apt install -y curl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+kubectl version --client
+
+# Install AWS CLI 
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+sudo apt install -y unzip
+unzip -q awscliv2.zip
+sudo ./aws/install
+
+echo "Installation completed successfully."
